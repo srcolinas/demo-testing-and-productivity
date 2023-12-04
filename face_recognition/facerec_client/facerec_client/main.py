4978a71b9d2ed5116df9e7e5fb3d3394137ed8f7
Sebastian Rodriguez Colina <srcolinas@gmail.com>
2023-12-04 13:55:31 -0500

    fix

--- face_recognition/facerec_client/facerec_client/main.py
+++ face_recognition/facerec_client/facerec_client/main.py
@@ -7,6 +7,7 @@ from facerec_pb2s import facerec_pb2
 from facerec_pb2s import facerec_pb2_grpc
 
 from facerec_client.image_producer import produce_images
+from facerec_client.keyboard_handler import read_signal
 from facerec_client.opencv_visualizer import OpenCVVisualizer
 
 
@@ -15,11 +16,11 @@ def run(producer: Iterable[bytes], address: str = "localhost:50051"):
         stub = facerec_pb2_grpc.FaceRecognitionStub(channel)
         for img in producer:
             response = stub.run(facerec_pb2.Payload(image=img))
-        print(f"{response}")
+            print(f"{response}")
 
 
 def _main():
-    run(produce_images(cv2.VideoCapture(0), OpenCVVisualizer()))
+    run(produce_images(cv2.VideoCapture(0), OpenCVVisualizer(), read_signal))
 
 
 if __name__ == "__main__":
